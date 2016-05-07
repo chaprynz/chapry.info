8167acb69fa5b05b15fc4fac1a4b3ef691277e3d
chaprynz <henrysai89@gmail.com>
2016-05-07 04:36:30 +0000

    deploy

--- config/initializers/paperclip.rb
+++
@@ -1,2 +0,0 @@
-Paperclip::Attachment.default_options[:url] = ':chaprydeploy.s3-us-east-2.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
