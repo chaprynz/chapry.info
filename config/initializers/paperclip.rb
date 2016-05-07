e75fc1c538f8c2b15224a1a4385801b9b0e0c290
chaprynz <henrysai89@gmail.com>
2016-05-07 01:36:11 +0000

    deploy

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = 'chaprydeploy.s3-accelerate.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 'chaprydeploy.s3-accelerate.amazonaws.com'
