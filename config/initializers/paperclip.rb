40d8f4332639e4171e0c8893092a455f6d7cf02f
chaprynz <henrysai89@gmail.com>
2016-05-07 04:17:30 +0000

    deploy

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,2 @@
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+Paperclip::Attachment.default_options[:url] = ':chaprydeploy.s3-us-east-2.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
