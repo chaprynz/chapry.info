cd589587da972b54f0718d4baa9833ec79ec88c5
chaprynz <henrysai89@gmail.com>
2016-07-24 08:05:52 +0000

    change version

--- app/controllers/tintuc_controller.rb
+++ app/controllers/tintuc_controller.rb
@@ -2,7 +2,7 @@ class TintucController < ApplicationController
   def show
     category = Category.find(params[:category_id])
     @tintuc = Tintuc.find(params[:id])
-    @tintucs = category.tintucs.order(created_at: :desc).page(params[:page]).per(7)
+    @tintucs = category.tintucs.order(created_at: :desc).page(params[:page]).per(9)
     @categories = Category.all
   end
 end
